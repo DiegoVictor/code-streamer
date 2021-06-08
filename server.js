64198851507df55abacb3054b7cde73a32ca8f11
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-06-08 20:04:19 -0300

    Validate range header

--- server.js
+++ server.js
@@ -6,4 +6,15 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, "public")));
 app.use(express.json());
 
+app.get("/videos/:file", async (request, response) => {
+  const { file } = request.params;
+  const { range } = request.headers;
+
+  if (range) {
+    return response.status(400).json({
+      message: "Range header is required",
+    });
+  }
+});
+
 app.listen(3000);
