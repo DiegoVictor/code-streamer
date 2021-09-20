d962e0bf0309e9558dafc9c7543ef4422be232d1
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-09-20 18:30:59 -0300

    Increase chunk size

--- src/server.js
+++ src/server.js
@@ -32,7 +32,7 @@ app.get('/videos/:slug', async (request, response) => {
   }
   const { size: fileSize } = stat;
 
-  const chunkSize = 1024 * 32;
+  const chunkSize = 1024 * 48;
   const start = Number(range.replace(/\D/gi, ''));
   const end = Math.min(start + chunkSize, fileSize - 1);
 
